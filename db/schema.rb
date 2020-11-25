a6bb77fb9b4b9dc735b708d4b75552b85677ad2a
Cosmel Villalobos <cosmelvg@gmail.com>
2020-11-25 16:03:36 -0500

    added the comments table and relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_172457) do
+ActiveRecord::Schema.define(version: 2020_11_25_203616) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author_name"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
