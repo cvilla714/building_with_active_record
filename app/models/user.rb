bfe37d5a95f5addc8d78270bdfc3af0cffa46265
Cosmel Villalobos <cosmelvg@gmail.com>
2020-11-26 13:34:16 -0500

    added content before merging

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    has_many :posts
-    has_many :comments
-     validates :username, :email, :password, presence: true
-   validates :username, :email, uniqueness: true
-   validates :password, :username, length: { in: 5..10 }
+  has_many :posts
+  has_many :comments
+  validates :username, :email, :password, presence: true
+  validates :username, :email, uniqueness: true
+  validates :password, :username, length: { in: 5..10 }
 end
